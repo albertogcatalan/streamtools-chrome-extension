96436b5844525a7780232f8c809792cdadc93378
Alberto González <alberto@gcatalan.com>
2020-12-22 15:12:04 +0100

    leccion8

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-    let value = "Hola mundo";
-    chrome.storage.sync.set({message: value}, function() {
-        console.log('Nuestra variable es ' + value);
-    });
-});
--- options.js
+++ options.js
@@ -1,30 +1,19 @@
 function saveOptions() {
     var streamtoolsapikey = document.getElementById("streamtoolsapikey").value;
-    var widgetid = document.getElementById("widgetid").value;
-    var widgettype = document.getElementById("widgettype").value;
+    chrome.storage.local.set({streamtoolsapikey: streamtoolsapikey}, function() {
+        var msg = document.getElementById("message");
+        msg.textContent = "Success!";
+        msg.classList.remove("d-none");
 
-    chrome.storage.sync.set({widgetid: widgetid, widgettype: widgettype}, function() {
-        chrome.storage.local.set({streamtoolsapikey: streamtoolsapikey}, function() {
-            var msg = document.getElementById("message");
-            msg.textContent = "Guardado con éxito";
-
-            setTimeout(function() {
-                msg.textContent = '';
-            }, 2000);
-        });
+        setTimeout(function() {
+            msg.textContent = '';
+            msg.classList.add("d-none");
+        }, 2000);
     });
 }
 
 function loadOptions()
 {
-    chrome.storage.sync.get("widgetid", function(result) {
-        document.getElementById("widgetid").value = result.widgetid || '';
-    });
-
-    chrome.storage.sync.get("widgettype", function(result) {
-        document.getElementById("widgettype").value = result.widgettype || '';
-    });
-
     chrome.storage.local.get("streamtoolsapikey", function(result) {
         document.getElementById("streamtoolsapikey").value = result.streamtoolsapikey || '';
     });
